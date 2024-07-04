1bf836a10f6974a021869a7e4b62a5754457fa07
mibelmay <zemcovannastasia11@gmail.com>
2024-07-04 23:39:13 +0500

    smth

--- app/MapService.py
+++ app/MapService.py
@@ -1,10 +1,13 @@
+import math
+
 import branca
-from folium import folium, Marker, Popup, Icon
+from folium import folium, Marker, Popup, LatLngPopup
 from geopy import Nominatim
 import pandas as pd
+from jinja2 import Template
 from pandas import DataFrame
 from sklearn.cluster import KMeans
-from folium.plugins import MarkerCluster, MousePosition
+from folium.plugins import MarkerCluster
 
 
 class MapService:
@@ -20,6 +23,8 @@ class MapService:
         map_center = [55.1542, 61.4282]
         my_map = folium.Map(location=map_center, zoom_start=12, tiles='cartodbpositron')
 
+        GetLatLngPopup().add_to(my_map)
+
         for cluster_id in data['cluster'].unique():
             marker_cluster = MarkerCluster().add_to(my_map)
             cluster_data = data[data['cluster'] == cluster_id]
@@ -30,20 +35,6 @@ class MapService:
                     location=[cluster_data.latitude.iloc[i], cluster_data.longitude.iloc[i]],
                     popup=Popup(iframe, min_width=350, max_width=350)
                 ).add_to(marker_cluster)
-
-        formatter = "function(num) {return L.Util.formatNum(num, 5);};"
-        mouse_position = MousePosition(
-            position='topright',
-            separator=' Long: ',
-            empty_string='NaN',
-            lng_first=False,
-            num_digits=20,
-            prefix='Lat:',
-            lat_formatter=formatter,
-            lng_formatter=formatter,
-        )
-
-        my_map.add_child(mouse_position)
 
         my_map.get_root().render()
         header = my_map.get_root().header.render()
@@ -59,15 +50,40 @@ class MapService:
         return data
 
     @staticmethod
-    def get_dataframe(apartments) -> DataFrame:  # ИЗМЕНИТЬ КОГДА БУДЕТ ТАБЛИЦА LOCATIONS
+    def get_dataframe(apartments) -> DataFrame:
         data = pd.DataFrame(columns=['apartment', 'latitude', 'longitude'])
         count: int = 0
         for apartment in apartments:
-            geolocator = Nominatim(user_agent="Tester")
-            location = geolocator.geocode(apartment.address.replace('р-н', 'район'))
-            data.loc[count] = [apartment, location.latitude, location.longitude]
+            # geolocator = Nominatim(user_agent="Tester")
+            # location = geolocator.geocode(apartment.address.replace('р-н', 'район'))
+            data.loc[count] = [apartment, apartment.latitude, apartment.longitude]
             count += 1
         return data
+
+    @staticmethod
+    def get_apartments_in_radius(apartments, lat: float, long: float, radius_km=1):
+        new_data = []
+        for apartment in apartments:
+            if MapService.get_distance(lat, apartment.latitude, long, apartments.longitude) <= radius_km:
+                new_data.append(apartment)
+        return new_data
+
+    @staticmethod
+    def get_distance(lat1, lat2, long1, long2):
+        long1 = math.radians(long1)
+        long2 = math.radians(long2)
+        lat1 = math.radians(lat1)
+        lat2 = math.radians(lat2)
+
+        D_Lo = long2 - long1
+        D_La = lat2 - lat1
+        P = math.sin(D_La / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(D_Lo / 2) ** 2
+
+        Q = 2 * math.asin(math.sqrt(P))
+
+        R_km = 6371
+
+        return Q * R_km
 
     @staticmethod
     def create_popup(apartment):
@@ -91,3 +107,31 @@ class MapService:
          window.parent.postMessage(message, '*'); 
         };
         """
+
+
+class GetLatLngPopup(LatLngPopup):
+    _template = Template(u"""
+            {% macro script(this, kwargs) %}
+                var {{this.get_name()}} = L.popup();
+                function latLngClick(e) {
+                    fetch('/get_apartments_in_radius', {
+                        method: 'POST',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ latitude: e.latlng.lat.toFixed(7),
+                                                longitude: e.latlng.lng.toFixed(7) })
+                         })
+                      .then(response => {
+                        if (!response.ok) {
+                          throw new Error('Ошибка при отправке запроса');
+                        }
+                      })
+                    }
+                {{this._parent.get_name()}}.on('click', latLngClick);
+            {% endmacro %}
+            """)
+
+    def __init__(self):
+        super(GetLatLngPopup, self).__init__()
+        self._name = 'GetLatLngPopup'
